2c06512d3038e44777b713c78422c138336594d7
Crazycaleb <52386586+Crazycaleb@users.noreply.github.com>
2023-08-07 14:27:15 -0400

    Initial Commit

---
+++ Assets/OffKeysScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+using KModkit;
+
+public class OffKeysScript : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+    public KMSelectable[] Buttonage;
+    public KMSelectable[] displayButtons;
+    public Sprite[] Symbols;
+    public SpriteRenderer[] DisplaySymbols;
+
+
+
+    private int _moduleId;
+    private static int _moduleIdCounter = 1;
+    private bool _moduleSolved;
+
+
+    
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
+
