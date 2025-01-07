e1047cf7cfd3abd3f7eb82789c1446d6ed49cb55
Blananas2 <blananas2@gmail.com>
2025-01-06 23:42:40 -0500

    add a shitload of useless comments ;-;

--- Assets/OffKeysScript.cs
+++ Assets/OffKeysScript.cs
@@ -10,19 +10,19 @@ public class OffKeysScript : MonoBehaviour
     public KMBombModule Module;
     public KMBombInfo BombInfo;
     public KMAudio Audio;
-    public KMSelectable[] Buttonage;
+    public KMSelectable[] PianoKeySels;
     public Sprite[] Symbols;
     public SpriteRenderer[] SpriteSlots;
-    public KMSelectable[] ButtonageMach2;
+    public KMSelectable[] RuneSels;
 
     private int _moduleId;
     private static int _moduleIdCounter = 1;
     private bool _moduleSolved;
 
-    private int[] KeyValueage = {0,0,0,0,0,1,1,1,1,2,2,2};
+    private int[] KeyShuffle = {0,0,0,0,0,0,0,0,1,1,1,1};
     private List<int> FaultyKeys = new List<int> {};
     private int[] Offsets = new int[12];
-    private int[] Sym = new int[3];
+    private int[] PickedSymbols = new int[3];
     private int RuneSelected = -1;
     private int[] NotesToAssign = new int [3];
     private int[] Assignments = new int [4];
@@ -76,12 +76,12 @@ public class OffKeysScript : MonoBehaviour
     {
         _moduleId = _moduleIdCounter++;
 
-        foreach (KMSelectable button in Buttonage)
+        foreach (KMSelectable button in PianoKeySels)
         {
             button.OnInteract += delegate () { InputPress(button); return false; };
         }
 
-        foreach (KMSelectable Rune in ButtonageMach2)
+        foreach (KMSelectable Rune in RuneSels)
         {
             Rune.OnInteract += delegate () { RuneSelect(Rune); return false; };
         }
@@ -91,13 +91,13 @@ public class OffKeysScript : MonoBehaviour
 
     void GeneratePuzzle() 
     {
-        KeyValueage.Shuffle();
+        KeyShuffle.Shuffle(); // this has exactly 4 1s
 
-        for (int i = 0; i < KeyValueage.Length; i++)
+        for (int i = 0; i < KeyShuffle.Length; i++)
         {
-            if (KeyValueage[i] == 1)
+            if (KeyShuffle[i] == 1)
             {
-                FaultyKeys.Add(i);
+                FaultyKeys.Add(i); //the keys at the indexes of these 1s get put into FaultyKeys
             }
         }
 
@@ -105,82 +105,86 @@ public class OffKeysScript : MonoBehaviour
 
         for (int i = 0; i < 4; i++)
         {
-            Offsets[FaultyKeys[i]] = (Rnd.Range(0,2)==0) ? 1 : -1;
+            Offsets[FaultyKeys[i]] = (Rnd.Range(0,2)==0) ? 1 : -1; //these faulty keys play a note off by ±1 semitone at random
         }
 
-        List<int>[] CorKeys = Enumerable.Range(0, 4).Select(i => CalcKeys(FaultyKeys[i]).ToList()).ToArray();
+        List<int>[] SymbolFinder = Enumerable.Range(0, 4).Select(i => CalcKeys(FaultyKeys[i]).ToList()).ToArray(); //this turns the faulty keys to every possible symbol with that note as a corner using CalcKeys()
 
-        int[] KimWexler = new int[37];
-        for (int i = 0; i < KimWexler.Length; i++)
+        int[] PositionTally = new int[37]; //positionTally is a tally for all positions tracking how many faulty note corners are "hit"
+        for (int i = 0; i < PositionTally.Length; i++)
         {
             for (int j = 0; j < 4; j++)
             {
-                if (CorKeys[j].Contains(i))
+                if (SymbolFinder[j].Contains(i))
                 {
-                    KimWexler[i]++;
+                    PositionTally[i]++;
                 }
             }
         }
 
-        WompWomp:
-        Sym = Enumerable.Range(0, 37).Where(i => KimWexler[i] != 0).ToArray().Shuffle().Take(3).ToArray();
-        if (Sym.Any(i => RunesDiagram[i].Contains(FaultyKeys[3])))
-            goto WompWomp;
+        StartOver:
+        PickedSymbols = Enumerable.Range(0, 37).Where(i => PositionTally[i] != 0).ToArray().Shuffle().Take(3).ToArray(); //this randomly picks 3 symbols have at least one faulty note as a corner
+        if (PickedSymbols.Any(i => RunesDiagram[i].Contains(FaultyKeys[3]))) //we start over (choose new symbols) if any chosen symbol maps to the note furthest along the piano? why? shouldn't this be BELOW the next bit?
+            goto StartOver;
 
-        var Notes = Sym.Select(i => RunesDiagram[i].Where(j => FaultyKeys.Contains(j)).OrderBy(x => x).ToArray()).ToArray();
-        var oldNotes = Notes.ToArray();
+        var Notes = PickedSymbols.Select(i => RunesDiagram[i].Where(j => FaultyKeys.Contains(j)).OrderBy(x => x).ToArray()).ToArray(); //we keep track of the faulty notes at the symbols we chose ordered numerically? sure?
+        var OldNotes = Notes.ToArray();
 
-        if (Notes[0].SequenceEqual(Notes[1]) || Notes[0].SequenceEqual(Notes[2]) || Notes[1].SequenceEqual(Notes[2]))
-            goto WompWomp;
-        var list = new List<int>();
+        if (Notes[0].SequenceEqual(Notes[1]) || Notes[0].SequenceEqual(Notes[2]) || Notes[1].SequenceEqual(Notes[2])) //we cannot have two notes in notes which match eachother, restart if it happens. why do we not care about the submit note?
+            goto StartOver;
+        var list = new List<int>(); //okay so this list is going to have every note in Notes
         for (int i = 0; i < 3; i++)
             list.AddRange(Notes[i]);
-        if (list.Distinct().Count() != 3)
-            goto WompWomp;
+        if (list.Distinct().Count() != 3) //if the distinct number of notes in the list is anything other than 3 we start over
+            goto StartOver;
 
-        NotesToAssign = new int[3] { 99, 99, 99 };
 
-        if (!Notes.Any(i => i.Length == 1))
-            goto WompWomp;
+        NotesToAssign = new int[3] { 99, 99, 99 }; //we have values which keep track of which note the symbol must be assigned to
 
-        for (int loop = 0; loop < 3; loop++)
+        if (!Notes.Any(i => i.Length == 1)) //a note needs to be 1 to be the break-in to the puzzle, restart if this cannot happen
+            goto StartOver;
+
+        for (int loop = 0; loop < 3; loop++) //we go through this three times
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++) //of the big loop we go through each note
             {
-                if (NotesToAssign[i] != 99)
+                if (NotesToAssign[i] != 99) //don't change if current note to assign has been assigned
                     continue;
-                if (Notes[i].Length == 0)
-                    goto WompWomp;
-                if (Notes[0].SequenceEqual(Notes[1]) || Notes[0].SequenceEqual(Notes[2]) || Notes[1].SequenceEqual(Notes[2]))
-                    goto WompWomp;
-                if (Notes[i].Length == 1)
+                if (Notes[i].Length == 0) //if we cannot assign a current note we start over
+                    goto StartOver;
+                if (Notes[0].SequenceEqual(Notes[1]) || Notes[0].SequenceEqual(Notes[2]) || Notes[1].SequenceEqual(Notes[2])) //if notes match between any of them we have to start over
+                    goto StartOver;
+                if (Notes[i].Length == 1) //when we get to a single note which can be assigned
                 {
-                    NotesToAssign[i] = Notes[i][0];
-                    Notes[(i + 1) % 3] = Notes[(i + 1) % 3].Where(j => j != NotesToAssign[i]).ToArray();
+                    NotesToAssign[i] = Notes[i][0]; //mark off assigned note as the one we have
+                    Notes[(i + 1) % 3] = Notes[(i + 1) % 3].Where(j => j != NotesToAssign[i]).ToArray(); //and remove it as possibilities from the other two
                     Notes[(i + 2) % 3] = Notes[(i + 2) % 3].Where(j => j != NotesToAssign[i]).ToArray();
-                    goto nextIter;
+                    goto nextIter; //and immediately go back to the first note, move onto the next one in the big loop from the top
                 }
             }
             nextIter:;
         }
+        //i guess it just works if we make it down here, i can believe that
+
         Debug.LogFormat("[Off Keys #{0}] The Faulty keys are: {1}", _moduleId, FaultyKeys.Select(i => Piano[i]).Join(", "));
-        Debug.LogFormat("[Off Keys #{0}] The Runes are: {1}", _moduleId, Sym.Join(", "));
-        Debug.Log("Notes: " + oldNotes.Select(i => i.Select(j => Piano[j]).Join(" ")).Join(", "));
+        Debug.LogFormat("[Off Keys #{0}] The Runes are: {1}", _moduleId, PickedSymbols.Join(", "));
+        Debug.Log("Notes: " + OldNotes.Select(i => i.Select(j => Piano[j]).Join(" ")).Join(", "));
         Debug.LogFormat("[Off Keys #{0}] The note for each rune should be: {1}", _moduleId, NotesToAssign.Select(i => Piano[i]).Join(", "));
         Debug.LogFormat("[Off Keys #{0}] The key to submit should be: {1}", _moduleId, FaultyKeys.Where(x => !NotesToAssign.Contains(x)).Select(x => Piano[x]).Single());
     }
+    //WOW BLAN WASTED HIS TIME WRITING COMMENTS FOR THE ONE PERFECT (Clueless) PART OF THE MOD ;-; dsdhaldsajklk;sadjk
 
     private List<int> CalcKeys(int s)
     {
-        List<int> HankSchrader = new List<int> {};
+        List<int> FoundSymbols = new List<int> {};
         for (int i = 0; i < 37; i++)
         {
             if (RunesDiagram[i].Contains(s))
             {
-                HankSchrader.Add(i);
+                FoundSymbols.Add(i);
             }
         }
-        return HankSchrader;
+        return FoundSymbols;
 
     }
 
@@ -190,20 +194,20 @@ public class OffKeysScript : MonoBehaviour
         Rune.AddInteractionPunch();
         for (int i = 0; i < 3; i++)
         {
-            if (ButtonageMach2[i] == Rune)
-                RuneSelected = RuneSelected == i + 1 ? -1 : i + 1;
+            if (RuneSels[i] == Rune) {
+                RuneSelected = i + 1;
+            }
         }
-
     }
 
 
-    void InputPress(KMSelectable button)
+    void InputPress(KMSelectable button) //WHEN WE PRESS A KEY THIS IS BADLY NAMED hdsajjdakjfdkl;
     {
         if (_moduleSolved || solvePlaying != null) { return; }
         button.AddInteractionPunch();
         for (int i = 0; i < 12; i++)
         {
-            if (Buttonage[i]==button)
+            if (PianoKeySels[i]==button)
             {
                 if (RuneSelected == -1 && !IsThisTheSubmitButtonLol(i))
                 {   
@@ -302,11 +306,11 @@ public class OffKeysScript : MonoBehaviour
     private IEnumerator DisplaySymbols()
     {
         yield return new WaitForSeconds(0.35f);
-        SpriteSlots[0].sprite = Symbols[Sym[0]];
+        SpriteSlots[0].sprite = Symbols[PickedSymbols[0]];
         yield return new WaitForSeconds(0.35f);
-        SpriteSlots[1].sprite = Symbols[Sym[1]];
+        SpriteSlots[1].sprite = Symbols[PickedSymbols[1]];
         yield return new WaitForSeconds(0.35f);
-        SpriteSlots[2].sprite = Symbols[Sym[2]];
+        SpriteSlots[2].sprite = Symbols[PickedSymbols[2]];
         yield return new WaitForSeconds(0.35f);
         SpriteSlots[0].sprite = Symbols[37];
         yield return new WaitForSeconds(0.35f);
@@ -353,9 +357,9 @@ public class OffKeysScript : MonoBehaviour
 
         if (split[0].ContainsIgnoreCase("CYCLE"))
         {
-            for (int i = 0; i < Buttonage.Length; i++)
+            for (int i = 0; i < PianoKeySels.Length; i++)
             {
-                Buttonage[i].OnInteract();
+                PianoKeySels[i].OnInteract();
                 yield return new WaitForSeconds(2);
             }
             yield break;
@@ -396,9 +400,9 @@ public class OffKeysScript : MonoBehaviour
             if (split[2].Length > 2)
                 yield break;
 
-            ButtonageMach2[int.Parse(split[1]) - 1].OnInteract();
+            RuneSels[int.Parse(split[1]) - 1].OnInteract();
             yield return new WaitForSeconds(0.1f);
-            Buttonage[Array.IndexOf(Piano, split[2])].OnInteract();
+            PianoKeySels[Array.IndexOf(Piano, split[2])].OnInteract();
             yield return new WaitForSeconds(0.1f);
             yield break;
         }
@@ -408,7 +412,7 @@ public class OffKeysScript : MonoBehaviour
             if (split[0].Length > 2)
                 yield break;
 
-            Buttonage[Array.IndexOf(Piano, split[0])].OnInteract();
+            PianoKeySels[Array.IndexOf(Piano, split[0])].OnInteract();
             yield return new WaitForSeconds(0.1f);
         }
     }
@@ -419,7 +423,7 @@ public class OffKeysScript : MonoBehaviour
 
         if (RuneSelected != -1)
         {
-            ButtonageMach2[RuneSelected].OnInteract();
+            RuneSels[RuneSelected].OnInteract();
             yield return new WaitForSeconds(0.1f);
         }
 
@@ -434,13 +438,13 @@ public class OffKeysScript : MonoBehaviour
 
         for (int i = 0; i < 3; i++)
         {
-            ButtonageMach2[i].OnInteract();
+            RuneSels[i].OnInteract();
             yield return new WaitForSeconds(0.1f);
-            Buttonage[NotesToAssign[i]].OnInteract();
+            PianoKeySels[NotesToAssign[i]].OnInteract();
             yield return new WaitForSeconds(0.1f);
         }
 
-        Buttonage[FaultyKeys.Where(x => !NotesToAssign.Contains(x)).Single()].OnInteract();
+        PianoKeySels[FaultyKeys.Where(x => !NotesToAssign.Contains(x)).Single()].OnInteract();
         yield return new WaitForSeconds(0.1f);
 
         while (!_moduleSolved)
